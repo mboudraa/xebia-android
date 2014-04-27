6ef5cfd4d0156cc4b9a0db1d604730e4eeae0c2c
Mounir Boudraa <mounir.boudraa@gmail.com>
2014-04-27 17:09:50 +0200

    refacto syntax highlight

--- xebia-app/src/main/java/fr/xebia/app/PostListActivity.java
+++ xebia-app/src/main/java/fr/xebia/app/PostListActivity.java
@@ -5,7 +5,6 @@ import android.graphics.RectF;
 import android.graphics.drawable.Drawable;
 import android.os.Build;
 import android.view.View;
-import android.view.ViewTreeObserver;
 import android.view.animation.AccelerateDecelerateInterpolator;
 import android.widget.AbsListView;
 import android.widget.FrameLayout;
--- xebia-app/src/main/java/fr/xebia/app/prettify/PrettifyHighlighter.java
+++ xebia-app/src/main/java/fr/xebia/app/prettify/PrettifyHighlighter.java
@@ -1,6 +1,5 @@
 package fr.xebia.app.prettify;
 
-import android.text.Html;
 import fr.xebia.app.prettify.syntaxhighlight.ParseResult;
 import fr.xebia.app.prettify.syntaxhighlight.Parser;
 
@@ -25,7 +24,7 @@ public final class PrettifyHighlighter {
         for (ParseResult result : results) {
             String type = result.getStyleKeys().get(0);
             String content = sourceCode.substring(result.getOffset(), result.getOffset() + result.getLength());
-            String newContent = String.format(FONT_PATTERN, getColor(type), Html.escapeHtml(content));
+            String newContent = String.format(FONT_PATTERN, getColor(type), escapeHtml(content));
 
             newContent = newContent.replaceAll("&#13;&#10;", "<br>");
             newContent = newContent.replaceAll("<font color=\"#ffffff\"><br></font>", "<br>");
@@ -52,5 +51,46 @@ public final class PrettifyHighlighter {
         map.put("pln", "ffffff");
         map.put("tag", "d68a4e");
         return map;
+    }
+
+    private static String escapeHtml(CharSequence text) {
+        StringBuilder out = new StringBuilder();
+        withinStyle(out, text, 0, text.length());
+        return out.toString();
+    }
+
+    private static void withinStyle(StringBuilder out, CharSequence text,
+                                    int start, int end) {
+        for (int i = start; i < end; i++) {
+            char c = text.charAt(i);
+
+            if (c == '<') {
+                out.append("&lt;");
+            } else if (c == '>') {
+                out.append("&gt;");
+            } else if (c == '&') {
+                out.append("&amp;");
+            } else if (c >= 0xD800 && c <= 0xDFFF) {
+                if (c < 0xDC00 && i + 1 < end) {
+                    char d = text.charAt(i + 1);
+                    if (d >= 0xDC00 && d <= 0xDFFF) {
+                        i++;
+                        int codepoint = 0x010000 | (int) c - 0xD800 << 10 | (int) d - 0xDC00;
+                        out.append("&#").append(codepoint).append(";");
+                    }
+                }
+            } else if (c > 0x7E || c < ' ') {
+                out.append("&#").append((int) c).append(";");
+            } else if (c == ' ') {
+                while (i + 1 < end && text.charAt(i + 1) == ' ') {
+                    out.append("&nbsp;");
+                    i++;
+                }
+
+                out.append(' ');
+            } else {
+                out.append(c);
+            }
+        }
     }
 }
